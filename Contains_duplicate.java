147644e3b85d5bb799d6b1071c87fe37462875b9
sadhanasadhu07 <sadhanaarumugam07@gmail.com>
2026-06-15 22:22:19 +0530

    Contains_duplicate, Valid_palindrome

---
+++ Contains_duplicate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Contains_duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
